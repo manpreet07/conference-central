192c1dfcd2aa3e2777acbaaa68ecacc224e9df01
Manpreet Singh <manpreetsingh@Manpreets-MBP.home>
2016-05-09 20:18:58 -0400

    added code for announcements

---
+++ conference_central/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import webapp2
+
+from conference import ConferenceApi
+from google.appengine.api import app_identity
+from google.appengine.api import mail
+
+
+class SetAnnouncementHandler(webapp2.RequestHandler):
+    def get(self):
+        """Set Announcement in Memcache."""
+        # TODO 1
+        # use _cacheAnnouncement() to set announcement in Memcache
+
+app = webapp2.WSGIApplication([
+    ('/crons/set_announcement', SetAnnouncementHandler),
+], debug=True)
